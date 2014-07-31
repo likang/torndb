06fda39f01bf7ae49648cbd32284369750b7f79a
likang <i@likang.me>
2014-07-31 15:39:10 +0800

    MySQL Connector Wrapper

--- torndb.py
+++ torndb.py
@@ -23,27 +23,17 @@ as torndb.
 
 from __future__ import absolute_import, division, with_statement
 
-import copy
-import itertools
 import logging
-import os
 import time
 
-try:
-    import MySQLdb.constants
-    import MySQLdb.converters
-    import MySQLdb.cursors
-except ImportError:
-    # If MySQLdb isn't available this module won't actually be useable,
-    # but we want it to at least be importable on readthedocs.org,
-    # which has limitations on third-party modules.
-    if 'READTHEDOCS' in os.environ:
-        MySQLdb = None
-    else:
-        raise
+import mysql.connector
+import mysql.connector.cursor
+import mysql.connector.errors
 
-version = "0.3"
-version_info = (0, 3, 0, 0)
+
+version = "0.4"
+version_info = (0, 4, 0, 0)
+
 
 class Connection(object):
     """A lightweight wrapper around MySQLdb DB-API connections.
@@ -59,26 +49,30 @@ class Connection(object):
     are very similar to the DB-API.
 
     We explicitly set the timezone to UTC and assume the character encoding to
-    UTF-8 (can be changed) on all connections to avoid time zone and encoding errors.
+    UTF-8 (can be changed) on all connections to avoid time zone
+    and encoding errors.
 
-    The sql_mode parameter is set by default to "traditional", which "gives an error instead of a warning"
-    (http://dev.mysql.com/doc/refman/5.0/en/server-sql-mode.html). However, it can be set to
-    any other mode including blank (None) thereby explicitly clearing the SQL mode.
+    The sql_mode parameter is set by default to "traditional",
+    which "gives an error instead of a warning"
+    (http://dev.mysql.com/doc/refman/5.0/en/server-sql-mode.html).
+    However, it can be set to any other mode including blank (None)
+    thereby explicitly clearing the SQL mode.
     """
     def __init__(self, host, database, user=None, password=None,
-                 max_idle_time=7 * 3600, connect_timeout=0, 
-                 time_zone="+0:00", charset = "utf8", sql_mode="TRADITIONAL"):
+                 max_idle_time=7 * 3600,
+                 autocommit=True,
+                 time_zone="+0:00", charset="utf8", sql_mode="TRADITIONAL"):
         self.host = host
         self.database = database
         self.max_idle_time = float(max_idle_time)
 
-        args = dict(conv=CONVERSIONS, use_unicode=True, charset=charset,
-                    db=database, init_command=('SET time_zone = "%s"' % time_zone),
-                    connect_timeout=connect_timeout, sql_mode=sql_mode)
+        args = dict(use_unicode=True, charset=charset,
+                    db=database, time_zone=time_zone, autocommit=autocommit,
+                    sql_mode=sql_mode)
         if user is not None:
             args["user"] = user
         if password is not None:
-            args["passwd"] = password
+            args["password"] = password
 
         # We accept a path to a MySQL socket file or a host(:port) string
         if "/" in host:
@@ -114,13 +108,12 @@ class Connection(object):
     def reconnect(self):
         """Closes the existing database connection and re-opens it."""
         self.close()
-        self._db = MySQLdb.connect(**self._db_args)
-        self._db.autocommit(True)
+        self._db = mysql.connector.connect(**self._db_args)
 
     def iter(self, query, *parameters, **kwparameters):
         """Returns an iterator for the given query and parameters."""
         self._ensure_connected()
-        cursor = MySQLdb.cursors.SSCursor(self._db)
+        cursor = self._cursor()
         try:
             self._execute(cursor, query, parameters, kwparameters)
             column_names = [d[0] for d in cursor.description]
@@ -131,11 +124,12 @@ class Connection(object):
 
     def query(self, query, *parameters, **kwparameters):
         """Returns a row list for the given query and parameters."""
-        cursor = self._cursor()
+        self._ensure_connected()
+        cursor = mysql.connector.cursor.MySQLCursorBuffered(self._db)
         try:
             self._execute(cursor, query, parameters, kwparameters)
             column_names = [d[0] for d in cursor.description]
-            return [Row(itertools.izip(column_names, row)) for row in cursor]
+            return [Row(zip(column_names, row)) for row in cursor]
         finally:
             cursor.close()
 
@@ -221,7 +215,7 @@ class Connection(object):
         # case by preemptively closing and reopening the connection
         # if it has been idle for too long (7 hours by default).
         if (self._db is None or
-            (time.time() - self._last_use_time > self.max_idle_time)):
+                (time.time() - self._last_use_time > self.max_idle_time)):
             self.reconnect()
         self._last_use_time = time.time()
 
@@ -232,7 +226,7 @@ class Connection(object):
     def _execute(self, cursor, query, parameters, kwparameters):
         try:
             return cursor.execute(query, kwparameters or parameters)
-        except OperationalError:
+        except mysql.connector.errors.OperationalError:
             logging.error("Error connecting to MySQL on %s", self.host)
             self.close()
             raise
@@ -245,20 +239,3 @@ class Row(dict):
             return self[name]
         except KeyError:
             raise AttributeError(name)
-
-if MySQLdb is not None:
-    # Fix the access conversions to properly recognize unicode/binary
-    FIELD_TYPE = MySQLdb.constants.FIELD_TYPE
-    FLAG = MySQLdb.constants.FLAG
-    CONVERSIONS = copy.copy(MySQLdb.converters.conversions)
-
-    field_types = [FIELD_TYPE.BLOB, FIELD_TYPE.STRING, FIELD_TYPE.VAR_STRING]
-    if 'VARCHAR' in vars(FIELD_TYPE):
-        field_types.append(FIELD_TYPE.VARCHAR)
-
-    for field_type in field_types:
-        CONVERSIONS[field_type] = [(FLAG.BINARY, str)] + CONVERSIONS[field_type]
-
-    # Alias some common MySQL exceptions
-    IntegrityError = MySQLdb.IntegrityError
-    OperationalError = MySQLdb.OperationalError
